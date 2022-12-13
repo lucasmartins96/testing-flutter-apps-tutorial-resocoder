a808635f8b1fbc7639916978af29f4664e63af34
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2022-12-13 15:58:11 -0300

    test(news_change_notifier): initial setup

---
+++ lib/test/news_change_notifier_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_testing_tutorial/news_change_notifier.dart';
+import 'package:flutter_testing_tutorial/news_service.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MockNewsService extends Mock implements NewsService {}
+
+void main() {
+  late NewsChangeNotifier sut;
+  late MockNewsService mockNewsService;
+
+  setUp(() {
+    mockNewsService = MockNewsService();
+    sut = NewsChangeNotifier(mockNewsService);
+  });
+}
